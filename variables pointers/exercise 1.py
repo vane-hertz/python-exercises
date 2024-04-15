c18a70d4a805d7149c5e79f52a7bd3a22a449379
vane-hertz <vainehertz@yahoo.com>
2024-04-15 16:58:20 -0700

    completed more stuff section

---
+++ variables pointers/exercise 1.py
@@ -0,0 +1,9 @@
+'''In your own words, explain the difference between these two expressions.
+'''
+
+my_object1 == my_object2
+my_object1 is my_object2
+
+'''The first is checking if the objects are equal in value,
+if the equality operator returns true on the values of the object
+the second is checking if the two variables reference the same object.
